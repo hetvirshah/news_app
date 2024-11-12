aa85e3b864dcbcbf30938a5841d64ba0b5699d1f
hetvi776 <hetvi@shreesoftech.com>
2024-11-12 12:05:40 +0530

    changes

--- lib/ui/screens/latestnews.dart
+++ lib/ui/screens/latestnews.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news/cubits/news_cubit.dart';
-import 'package:news/ui/widgets/news_card.dart';
+import 'package:news/ui/screens/widgets/news_card.dart';
 import 'package:shimmer/shimmer.dart';
 
 @immutable
